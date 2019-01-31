5894b7817f42311036d161f1a9038f8eac498b13
Victoria_Kiriyenko <Victoria_Kiriyenko@EPUALVIW0767.kyiv.epam.com>
2019-01-31 11:41:48 +0200

    Task Write a small calculator console application that will follow next requirements:
    accepts numbers from -20 to 20, if more than 20 or less -20 - returns a
    warning message.
    
    Build application using maven
    
    write results to a file with date/time of calculation
    
    arrange application structure according to OOP
    
    Create your personal Git repo with the results and application sources

---
+++ Calculator/src/Number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Number {
+	double n;
+	Scanner fromUser = new Scanner (System.in);
+
+Double getNumber() {
+	System.out.println("Enter a number in range [-20 to 20]: ");
+	n = Double.parseDouble(fromUser.nextLine());
+	return n;
+}
+void setNumber(double n) {
+	if(n>20 && n<-20) {
+		System.out.println("Enter a valid number");
+	} else {
+		this.n = n;
+	}
+}
+}
+
+
